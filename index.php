fcc31eb7cb587e048725d3ca629d4d97ef2cf6de
tom <tom@sapletta.com>
2021-05-05 17:16:57 +0200

    webscreen implementation

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 //use Monolog\Utils;
 //use Psr\Log\LogLevel;
 
-//require("../load_func.php");
+require("load_func.php");
 //require("../libs.php");
 
 
 //index.php
-
 $screen_shot_image = '';
+
 /*
 if (isset($_POST["screen_shot"])) {
 //    $domain = $_POST["url"];
@@ -32,7 +32,6 @@ if (isset($_POST["screen_shot"])) {
 }
 */
 if (isset($_POST["multi"])) {
-
     load_func([
         'https://php.letjson.com/let_json.php',
         'https://php.defjson.com/def_json.php',
@@ -41,17 +40,29 @@ if (isset($_POST["multi"])) {
     ], function () {
 
         $domains = $_POST["domains"];
+        // filter
+        $domains = str_replace("https://", "", $domains);
+        $domains = str_replace("http://", "", $domains);
+        $domains = str_replace("//", "", $domains);
+        $domains = str_replace("/", "", $domains);
 
 //        $domain_list = explode("\n", str_replace("\r", "", $domains));
 //        $domain_list = explode(PHP_EOL, $domains);
+        $domains = trim($domains);
+
+        if (empty($domains)) {
+            throw new Exception("domains is empty");
+        }
+
         $domain_list = array_values(array_filter(explode(PHP_EOL, $domains)));
 
+//        var_dump($domain_list);
+//        die;
         if (empty($domain_list)) {
             throw new Exception("domain list is empty");
         }
 
         $domain_nameserver_list = each_func($domain_list, function ($domain) {
-
 
             if (empty($domain)) return null;
 
@@ -63,9 +74,12 @@ if (isset($_POST["multi"])) {
             return "<img src=\"" . $base64 . "\" class='img-responsive img-thumbnail'/>";
         });
 
-    var_dump($domain_nameserver_list);
+        global $screen_shot_image;
 
         $screen_shot_image = implode("<br>", $domain_nameserver_list);
+//        var_dump($domain_nameserver_list);
+//        var_dump($screen_shot_image);
+
     });
 }
 
@@ -88,22 +102,21 @@ if (isset($_POST["multi"])) {
 <body>
 <div class="container box">
     <br/>
-    <h2 align="center">How to capture website screen shot from url in php</h2><br/>
+    <h2 align="center">capture website screen shot from domain name</h2><br/>
     <form method="post">
         <div class="form-group">
             <!--            <label>Enter URL</label>-->
             <label>Enter Domain name</label>
             <!--            <input type="url" name="url" class="form-control input-lg" required autocomplete="off"-->
             <!--                   value="http://softreck.com"/>-->
-<!--            <input type="domain" name="domain" class="input-lg" required autocomplete="on"-->
-<!--                   value="--><?php //echo $_POST["domain"] ?><!--"/>-->
+            <!--            <input type="domain" name="domain" class="input-lg" required autocomplete="on"-->
+            <!--                   value="--><?php //echo $_POST["domain"] ?><!--"/>-->
 
-            <textarea name="domains" cols="55">softreck.pl
-softreck.com
-            </textarea>
+            <textarea name="domains" cols="55" rows="20">softreck.pl
+softreck.com</textarea>
         </div>
         <br/>
-<!--        <input type="submit" name="screen_shot" value="Take a Screenshot" class="btn btn-info btn-lg"/>-->
+        <!--        <input type="submit" name="screen_shot" value="Take a Screenshot" class="btn btn-info btn-lg"/>-->
         <input type="submit" name="multi" value="Take a Screenshot" class="btn btn-info btn-lg"/>
     </form>
     <br/>
