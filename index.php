8e112be597df2b1641db309b341a1cd926341023
tom <tom@sapletta.com>
2021-05-06 17:22:19 +0200

    dnsy

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if(empty($_POST["domains"])){
     $_POST["domains"] = "softreck.com";
 }
 
+$dns = [];
 if (isset($_POST["multi"])) {
     load_func([
         'https://php.letjson.com/let_json.php',
@@ -95,7 +96,9 @@ if (isset($_POST["multi"])) {
 //            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
 //    <img src=\"" . $base64 . "\" class='img-responsive img-thumbnail'/>
+            global $dns;
             $dns[$urle] = getDNS($url);
+
             return "
 <div>
     SCREEN: <a href='$url_screen'> $url</a>
